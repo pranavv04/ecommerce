705718b5d824b7c821e7fed981a40581cd1eab64
Pranav Dhangar <113121206+pranavv04@users.noreply.github.com>
2024-10-30 23:29:30 +0530

    tired

--- server/server.js
+++ server/server.js
@@ -28,10 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", // Allow local development
-      "https://ecommerce-gamma-wine.vercel.app" // Allow hosted frontend
-    ],
+    origin: "*", // Allow all origins for testing
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -43,6 +40,7 @@ app.use(
     credentials: true,
   })
 );
+
 
 
 app.use(cookieParser());
